0963cbe668fcd0e0eeb1c4808d39e0a0fb926c5f
AlexMato <matontialessandro0@gmail.com>
2020-04-02 17:38:31 +0200

    prev e next di img e pallini completata

--- main.js
+++ main.js
@@ -7,25 +7,40 @@ $(document).ready(function(){
 
    function nextImg(){
     var imgActive = $(".images img.active");
+    var pallinoActive = $(".nav i.active");
     imgActive.removeClass("active");
+    pallinoActive.removeClass("active");
     if (imgActive.hasClass("last")){
       $(".images img.first").addClass("active");
     } else {
      imgActive.next().addClass("active");
     }
+    if (pallinoActive.hasClass("last")){
+      $(".nav i.first").addClass("active");
+    } else {
+     pallinoActive.next().addClass("active");
+    }
   }
-
+  //gestione del click su freccia sx
   $(".prev").click(
     prevImg
   );
-  //gestione del click su freccia sx
+
   function prevImg(){
     var imgActive = $(".images img.active");
+    var pallinoActive = $(".nav i.active");
     imgActive.removeClass("active");
+
     if (imgActive.hasClass("first")){
       $(".images img.last").addClass("active");
     } else {
      imgActive.prev().addClass("active");
+    }
+    pallinoActive.removeClass("active");
+    if (pallinoActive.hasClass("first")){
+      $(".nav i.last").addClass("active");
+    } else {
+     pallinoActive.prev().addClass("active");
     }
   }
 
